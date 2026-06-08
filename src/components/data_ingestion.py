769cde49c4c584ded1038fc5d0fa614b01a430cd
Shubhadeep Ghosh <ghoshshubhadeep18@gmail.com>
2026-06-09 00:15:46 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,9 @@ from src.pipeline.logger import logging
 import pandas as pd 
 from sklearn.model_selection import train_test_split 
 from dataclasses import dataclass 
+from src.components.data_transformation import DataTransformation 
+from src.components.data_transformation import DataTransformationConfig 
+
 
 @dataclass
 class DataIngestionConfig: 
@@ -17,7 +20,7 @@ class DataIngestion:
     def initiate_data_ingestion(self): 
         logging.info("Entered the data ingestion method or component") 
         try: 
-            df=pd.read_csv('notebook\data\Student Performance.csv') 
+            df = pd.read_csv('notebook\data\Student Performance.csv')
             logging.info("Read the dataset as dataframe") 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True) 
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True) 
@@ -31,5 +34,4 @@ class DataIngestion:
             raise CustomException(e, sys) 
 if __name__=="__main__": 
     obj=DataIngestion() 
-    obj.initiate_data_ingestion()        
-          
+    train_data, test_data=obj.initiate_data_ingestion()
