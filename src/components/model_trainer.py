2bdfd2096ce0667f7126f639ed5d2115e3b262cf
Shubhadeep Ghosh <ghoshshubhadeep18@gmail.com>
2026-06-11 01:16:51 +0530

    Prediction App

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -10,9 +10,9 @@ from xgboost import XGBRegressor
 from sklearn.linear_model import LinearRegression 
 from src.pipeline.exception import CustomException 
 from src.pipeline.logger import logging 
-from sklearn.neighbors import KNeighborsRegressor 
 from sklearn.tree import DecisionTreeRegressor 
-from src.pipeline.utils import save_object, evaluate_models
+from sklearn.neighbors import KNeighborsRegressor
+from src.pipeline.utils import save_object, evaluate_models 
 @dataclass
 class ModelTrainerConfig: 
     trained_model_file_path=os.path.join('artifacts', 'model.pkl') 
@@ -35,10 +35,43 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
                 "XGB Regressor": XGBRegressor(),
                 "Linear Regression": LinearRegression(),
-                "K-Neighbors Regressor": KNeighborsRegressor(),
-                "Decision Tree Regressor": DecisionTreeRegressor()
+                "Decision Tree Regressor": DecisionTreeRegressor(),
+                'kNN Regressor': KNeighborsRegressor()
             } 
-            model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models) 
+            params = {
+                "Decision Tree Regressor": {                        
+                    'criterion': [
+                        'squared_error',
+                        'friedman_mse',
+                        'absolute_error',
+                        'poisson'
+                    ],
+                },
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting Regressor": {                    
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regression": {},
+                "XGB Regressor": {                                  
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoosting Regressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor": {
+                    'learning_rate': [.1, .01, 0.5, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "kNN Regressor": {}                                 
+            }
+            model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, params=params) 
             best_model_score=max(sorted(model_report.values())) 
             best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)] 
             best_model=models[best_model_name] 
